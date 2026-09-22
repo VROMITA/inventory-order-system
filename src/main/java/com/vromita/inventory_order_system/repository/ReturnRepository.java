209d35e4add38885316976d6d9945e842d5c3556
Valerio <114232053+VROMITA@users.noreply.github.com>
2026-09-22 15:23:11 +0200

    feat: add Order, OrderItem and Return repositories

---
+++ src/main/java/com/vromita/inventory_order_system/repository/ReturnRepository.java
@@ -0,0 +1,13 @@
+package com.vromita.inventory_order_system.repository;
+
+import com.vromita.inventory_order_system.model.Return;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ReturnRepository extends JpaRepository<Return, Long> {
+
+    List<Return> findByOrderItemId(Long orderItemId);
+}
